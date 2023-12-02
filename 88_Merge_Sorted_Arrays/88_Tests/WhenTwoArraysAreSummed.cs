be13802e1066cf5d1104c8b738838777a2cef509
Michal <michalbuczek00@gmail.com>
2023-12-02 11:04:06 +0100

    Add project files.

---
+++ 88_Merge_Sorted_Arrays/88_Tests/WhenTwoArraysAreSummed.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace _88_Tests
+{
+    [TestFixture]
+    public class WhenTwoArraysAreSummed
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 0, 0, 0 }, 6, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 })]
+        [TestCase(new int[] { 1 }, 1, new int[] { }, 0, new int[] { 1 })]
+        [TestCase(new int[] { }, 0, new int[] { 1 }, 1, new int[] { 1 })]
+        public void ThenResultingArrayIsCorrect(int[] nums1, int m, int[] nums2, int n, int[] expected)
+        {
+           
+
+            //var nums1 = new[] { 1, 2, 3, 0, 0, 0 };
+            //var nums2 = new[] { 2, 5, 6 };
+            //var expected = new[] { 1, 2, 2, 3, 5, 6 };
+            #region Console writes
+            Console.Write("Nums1: ");
+            Console.WriteLine("[{0}]", string.Join(", ", nums1));
+
+            Console.Write("Nums2: ");
+            Console.WriteLine("[{0}]", string.Join(", ", nums2));
+
+            Console.Write("Expected: ");
+            Console.WriteLine("[{0}]", string.Join(", ", expected));
+            #endregion
+
+            var solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+
+            #region console writes
+            Console.Write("Result: ");
+            Console.WriteLine("[{0}]", string.Join(", ", nums1));
+            #endregion
+            nums1.Should().BeEquivalentTo(expected);
+            //Assert.AreEqual(nums1, expected);
+        }
+    }
+}
