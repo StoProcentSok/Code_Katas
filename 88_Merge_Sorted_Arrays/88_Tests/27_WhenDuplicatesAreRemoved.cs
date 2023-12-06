f83faec2e6a3d1d2a48fdd382868198587cabad3
Michal <michalbuczek00@gmail.com>
2023-12-06 19:08:38 +0100

    27 remove given element, return left count and starting array

---
+++ 88_Merge_Sorted_Arrays/88_Tests/27_WhenDuplicatesAreRemoved.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Katas;
+using NUnit;
+using NUnit.Framework;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    internal class _27_WhenGivenOccurencesAreRemoved
+    {
+        [Test]
+        [TestCase(new int[] {3,2,2,3}, 3, 2, new int[] {2,2} )]
+        [TestCase(new int[] {0,1,2,2,3,0,4,2}, 2, 5, new int[] {0,1,3,0,4} )]
+        public void ThenProperCountAndArrayIsReturned(int[] nums, int valueToRemove, int expectedLeftValues, int[] expectedNums)
+        {
+            var kata = new _27_Remove_Element();
+            var result = kata.RemoveElement(nums, valueToRemove);
+
+            result.Should().Be(expectedLeftValues);
+            nums.Should().StartWith(expectedNums);
+        }
+    }
+}
