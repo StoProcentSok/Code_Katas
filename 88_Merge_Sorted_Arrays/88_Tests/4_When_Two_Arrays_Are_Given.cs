a9cdf9be08f805c65c0cf62bb54824aab4f72a69
Michal <michalbuczek00@gmail.com>
2023-12-10 16:42:52 +0100

    Kat 4 added, tested, resolved/

---
+++ 88_Merge_Sorted_Arrays/88_Tests/4_When_Two_Arrays_Are_Given.cs
@@ -0,0 +1,22 @@
+﻿using Katas;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _4_When_Two_Arrays_Are_Given
+    {
+        [Test]
+        [TestCase(new int[] {1,3}, new int[] {2}, 2.0)]
+        [TestCase(new int[] {1,2}, new int[] {3, 4}, 2.5)]
+        [TestCase(new int[] {1,3}, new int[] {2, 7}, 2.5)]
+        public void ThenMedianIsReturned(int[] nums1, int[] nums2, double expected)
+        {
+            var kata = new _4_Median_Of_Two_Sorted();
+            var result = kata.FindMedianSortedArrays(nums1, nums2);
+
+            result.Should().Be(expected);
+        }
+    }
+}
