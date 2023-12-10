4d8c4b4b3daeeaf6cbec0678af5515ffeae0c418
Michal <michalbuczek00@gmail.com>
2023-12-10 19:53:43 +0100

    Added 4th Kata, not implemented.

---
+++ 88_Merge_Sorted_Arrays/88_Tests/4_When_Two_Lists_Are_Given.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _4_When_Two_Lists_Are_Given
+    {
+        [Test]
+        public void ThenListContainingSumsIsReturned()
+        {
+
+        }
+    }
+}
