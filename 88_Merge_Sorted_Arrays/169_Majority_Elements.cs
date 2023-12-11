d41494c3304243b2f3959e1ab2aa6db9d56aaa59
Michal <michalbuczek00@gmail.com>
2023-12-11 10:25:20 +0100

    Kata 169 prepared for resolving

---
+++ 88_Merge_Sorted_Arrays/169_Majority_Elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class _169_Majority_Elements
+    {
+        public int MajorityElement(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
---
+++ 88_Merge_Sorted_Arrays/88_Tests/169_When_Array_Is_Given.cs
@@ -0,0 +1,26 @@
+﻿using Katas;
+using NUnit.Framework;
+using System;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _169_When_Array_Is_Given
+    {
+        [Test]
+        [TestCase(new int[] {3, 2, 3}, 3)]
+        [TestCase(new int[] {2, 2, 1, 1, 2, 2}, 2)]
+        public void Then_Majority_Element_Is_Returned(int[] nums, int expected)
+        {
+            var Kata = new _169_Majority_Elements();
+            var result = Kata.MajorityElement(nums);
+
+            result.Should().Be(expected);
+        }
+    }
+}
