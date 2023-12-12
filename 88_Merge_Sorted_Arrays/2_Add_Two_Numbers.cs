6ceef3c2c5894e43a4669eff44edb3fa9220b146
Michal <michalbuczek00@gmail.com>
2023-12-12 22:37:22 +0100

    Kata 2 solved.

--- 88_Merge_Sorted_Arrays/2_Add_Two_Numbers.cs
+++ 88_Merge_Sorted_Arrays/2_Add_Two_Numbers.cs
@@ -10,37 +10,34 @@ namespace Katas
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            var result = new ListNode();
+            ListNode dummy_head = new ListNode(0);
+            ListNode current = dummy_head;
 
-            var trailingValue = 0;
-            var addend1 = l1;
-            var addend2 = l2;
+            int trailingValue = 0;
 
-
-
-            while (addend1 != null)
+            while (l1 != null || l2 != null)
             {
-                while(addend2 != null)
-                {
-                    
-                    var sum = addend1.val + addend2.val + trailingValue;
-                    if (sum >= 10)
-                    {
-                        result.val = 1;
-                        trailingValue = 10 - sum;
-                    }
-                    addend1 = l1.next;
-                    addend2 = l2.next;
-                }
+                int x = (l1 != null) ? l1.val : 0;
+                int y = (l2 != null) ? l2.val : 0;
+
+                int sum = x + y + trailingValue;
+                trailingValue = sum / 10;
+
+                current.next = new ListNode(sum % 10);
+                current = current.next;
+
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
             }
 
-            return result;
+            if (trailingValue > 0)
+            {
+                current.next = new ListNode(trailingValue);
+            }
+
+            return dummy_head.next;
         }
     }
-
-
-
-
 
     public class ListNode
     {
--- 88_Merge_Sorted_Arrays/88_Tests/2_When_Two_Lists_Are_Given.cs
+++ 88_Merge_Sorted_Arrays/88_Tests/2_When_Two_Lists_Are_Given.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using Katas;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,17 @@ namespace Kata_Tests
     public class _2_When_Two_Lists_Are_Given
     {
         [Test]
-        public void ThenListContainingSumsIsReturned()
+        [TestCase(new int[] {2,4,3}, new int[] {5,6,4}, new int[] {7,0,8})]
+        public void ThenListContainingSumsIsReturned(int[] l1Values, int[] l2Values, int[] resultValues)
         {
+            var kata = new _2_Add_Two_Numbers();
 
+            var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+            var result = kata.AddTwoNumbers(l1, l2);
+
+            result.Should().BeEquivalentTo(new ListNode(7, new ListNode(0, new ListNode(8))));
         }
     }
 }
