eec12d12fe44ab93fd0cbf2e8233ae93b78c05f0
Michal <michalbuczek00@gmail.com>
2023-12-13 16:04:27 +0100

    Moved projects to new, renamed folders

---
+++ Katas/135_Candy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class _135_Candy
+    {
+        public int Candy(int[] ratings)
+        {
+            var candiesGiven = 0;
+            var candiesGivenToPrevious = 0;
+            for (int i = 0; i < ratings.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (ratings[i +1] > ratings[i])
+                    {
+                        
+                    }
+                }
+            }
+
+            return candiesGiven;
+        }
+    }
+}
