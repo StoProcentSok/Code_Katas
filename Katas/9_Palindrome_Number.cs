ea4e39e0b4eac366d079764492dccf3f916ee20a
Michal <michalbuczek00@gmail.com>
2023-12-14 11:45:43 +0100

    Palindrome Kata done with use of strings..

---
+++ Katas/9_Palindrome_Number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class _9_Palindrome_Number
+    {
+        public bool IsPalindrome(int x) 
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            var number = x.ToString().ToCharArray();
+
+            var i = 0;
+            var j = number.Length - 1;
+
+            while (i <= j)
+            {
+                if (number[i] != number[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Katas_Tests/9_When_Number_Is_Given.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Katas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _9_When_Number_Is_Given
+    {
+        [Test]
+        [TestCase(-100, false)]
+        [TestCase(100, false)]
+        [TestCase(1, true)]
+        [TestCase(121, true)]
+        [TestCase(123, false)]
+        [TestCase(-121, false)]
+        public void ThenTrueIfNumberIsPalindrome(int number, bool isPalindrome)
+        {
+            var kata = new _9_Palindrome_Number();
+            var result = kata.IsPalindrome(number);
+
+            result.Should().Be(isPalindrome);
+        }
+    }
+}
