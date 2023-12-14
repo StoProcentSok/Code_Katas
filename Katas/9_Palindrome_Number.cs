ba02dc3a876cd8643b5e243bf108260bed2a564d
Michal <michalbuczek00@gmail.com>
2023-12-14 11:47:12 +0100

    .. and refactored to not use strings.

--- Katas/9_Palindrome_Number.cs
+++ Katas/9_Palindrome_Number.cs
@@ -10,27 +10,16 @@ namespace Katas
     {
         public bool IsPalindrome(int x) 
         {
-            if (x < 0)
+            var number = x;
+            var newNumber = 0;
+
+            while (number > 0)
             {
-                return false;
+                newNumber = newNumber * 10 + number % 10;
+                number /= 10;
             }
 
-            var number = x.ToString().ToCharArray();
-
-            var i = 0;
-            var j = number.Length - 1;
-
-            while (i <= j)
-            {
-                if (number[i] != number[j])
-                {
-                    return false;
-                }
-                i++;
-                j--;
-            }
-
-            return true;
+            return newNumber == x;
         }
     }
 }
