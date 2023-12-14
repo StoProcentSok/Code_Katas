89c48cb3250383c584b37dee38731889713e518c
Michal <michalbuczek00@gmail.com>
2023-12-14 15:35:05 +0100

    Kata 7 reverse integer done

---
+++ Katas/7_Reverse_Integer.cs
@@ -0,0 +1,27 @@
+﻿namespace Katas
+{
+    public class _7_Reverse_Integer
+    {
+        public int Reverse(int x)
+        {
+
+            var newValue = 0;
+            var dummy = x > 0 ? x : x * -1;
+
+            
+            while (dummy > 0)
+            {
+                var overflow = dummy % 10;
+                newValue = newValue * 10 + dummy % 10;
+                dummy = dummy / 10;
+
+                if (newValue%10 != overflow)
+                {
+                    return 0;
+                }
+            }
+
+            return x > 0 ? newValue : newValue * -1;
+        }
+    }
+}
---
+++ Katas_Tests/7_When_Integer_Is_Given.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Katas;
+using NUnit.Framework;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _7_When_Integer_Is_Given
+    {
+        [Test]
+        [TestCase(100, 1)]
+        [TestCase(123, 321)]
+        [TestCase(-231, -132)]
+        [TestCase(1534236469, 0)]
+        public void Then_Reverse_Integer_Is_Returned(int value, int expected)
+        {
+            var kata = new _7_Reverse_Integer();
+            var result = kata.Reverse(value);
+
+            result.Should().Be(expected);
+        }
+    }
+}
