cd432d80c45abef0aa8ac60d39b263250e09e35e
Michal <michalbuczek00@gmail.com>
2023-12-20 16:18:54 +0100

    Kata 1913 resolved. Array traverse solution beats 8 percent. Sort 5 percent.

---
+++ Katas/1913_Maximum_Product_Difference.cs
@@ -0,0 +1,12 @@
+﻿namespace Katas
+{
+    public class _1913_Maximum_Difference
+    {
+        public int MaxProductDifference(int[] nums)
+        {
+            Array.Sort(nums);
+            return (nums[nums.Length - 1] * nums[nums.Length - 2]
+                    - nums[0] * nums[1]);
+        }
+    }
+}
---
+++ Katas_Tests/1913_When_Numbers_Are_Given.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Katas;
+using NUnit.Framework;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _1913_When_Numbers_Are_Given
+    {
+        [Test]
+        [TestCase(new int[] { 5, 6, 2, 7, 4 }, 34)]
+        [TestCase(new int[] { 4, 2, 5, 9, 7, 4, 8 }, 64)]
+        public void Then_Maximum_Product_Difference_Is_Returned(int[] nums, int expected)
+        {
+            var kata = new _1913_Maximum_Difference();
+            var result = kata.MaxProductDifference(nums);
+
+            result.Should().Be(expected);
+        }
+    }
+}
