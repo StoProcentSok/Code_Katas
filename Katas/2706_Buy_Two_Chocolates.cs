6efe3c10f9b3e13ddb202dbf3b3f0dfb5661b9ab
Michal <michalbuczek00@gmail.com>
2023-12-25 13:22:28 +0100

    2706 Two Chockolates kata

---
+++ Katas/2706_Buy_Two_Chocolates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class _2706_Buy_Two_Chocolates
+    {
+        public int BuyChoco(int[] prices, int money)
+        {
+            //Trivial solution: Sort() prices, check two first prices.
+
+            var firstPrice = int.MaxValue;
+            var secondPrice = int.MaxValue;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < secondPrice)
+                {
+                    secondPrice = prices[i];
+                }
+                if (secondPrice < firstPrice)
+                {
+                    var temp = firstPrice;
+                    firstPrice = secondPrice;
+                    secondPrice = temp;
+                }
+            }
+
+            if (firstPrice + secondPrice <= money)
+            {
+                return money - firstPrice - secondPrice;
+            }
+
+            return money;
+        }
+    }
+}
---
+++ Katas_Tests/2706_When_Candies_Prices_And_Money_Is_Given.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Katas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _2706_When_Candies_Prices_And_Money_Is_Given
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 2 }, 3, 0)]
+        [TestCase(new int[] { 3, 2, 3}, 3, 3)]
+        public void ThenRemainingMoneyAfterBuyingTwoCheapestCandiesIsReturned(int[] prices, int money, int moneyReminder)
+        {
+            var kata = new _2706_Buy_Two_Chocolates();
+            var result = kata.BuyChoco(prices, money);
+
+            result.Should().Be(moneyReminder);
+        }
+    }
+}
