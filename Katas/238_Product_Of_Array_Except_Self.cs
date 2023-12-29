432c50fc450fceccc67994f216b582ca88748eb3
Michal <michalbuczek00@gmail.com>
2023-12-29 11:05:58 +0100

    Kata 238 TestCase 18 failing.

---
+++ Katas/238_Product_Of_Array_Except_Self.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class _238_Product_Of_Array_Except_Self
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var result = new int[nums.Length];
+            Array.Fill(result, 1);
+
+            var i = 0;
+            while (i < nums.Length)
+            {
+                for (var j = i - 1; j > -1; j--)
+                {
+                    result[i] = result[i] * nums[j];
+                }
+
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    result[i] = result[i] * nums[j];
+                }
+
+                i++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Katas_Tests/238_When_Array_Is_Given.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Katas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _238_When_Array_Is_Given
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4 }, new int[] { 24, 12, 8, 6 })]
+        [TestCase(new int[] { -1, 1, 0, -3, 3 }, new int[] { 0, 0, 9, 0, 0 })]
+        public void Then_Array_Of_Product_WIthout_Selfs_Is_Returned(int[] nums, int[] expected)
+        {
+            var kata = new _238_Product_Of_Array_Except_Self();
+            var result = kata.ProductExceptSelf(nums);
+
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
