2fe7c9aa2a933dbf195a0f0f9a0b696a93e641fa
Michal <michalbuczek00@gmail.com>
2024-01-05 15:13:52 +0100

    Kata 219

---
+++ Katas/219_Contains_Duplicate_II.cs
@@ -0,0 +1,27 @@
+﻿namespace Katas
+{
+    public class _219_Contains_Duplicate_II
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var set = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (set.Contains(nums[i]))
+                {
+                    return true;
+                }
+
+                set.Add(nums[i]);
+
+                if (set.Count > k)
+                {
+                    set.Remove(nums[i - k]);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Katas_Tests/219_When_Nums_Arr_Is_Given.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Katas;
+using NUnit.Framework;
+
+namespace Kata_Tests
+{
+    [TestFixture]
+    public class _219_When_Nums_Arr_Is_Given
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 1 }, 3, true)]
+        [TestCase(new int[] { 1, 0, 1, 1 }, 1, true)]
+        [TestCase(new int[] { 1, 2, 3, 1, 2, 3 }, 2, false)]
+        public void Then_True_If_Duplicates_Fullfill_Rule(int[] nums, int k, bool expected)
+        {
+            var kata = new _219_Contains_Duplicate_II();
+            var result = kata.ContainsNearbyDuplicate(nums, k);
+
+            result.Should().Be(expected);
+        }
+    }
+}
