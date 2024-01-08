971126d34035b3441d16a65db984895f66dd2085
Michal <michalbuczek00@gmail.com>
2024-01-08 14:48:07 +0100

    Kata 58 resolved, string split options merged with pipe operator

---
+++ Katas/58_Length_Of_Last_Word.cs
@@ -0,0 +1,11 @@
+﻿namespace Katas
+{
+    public class _58_Length_Of_Last_Word
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var words = s.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            return words.Last().Length;
+        }
+    }
+}
